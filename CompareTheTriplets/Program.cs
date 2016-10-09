47fe3fba1edf9b41717af991cd425a3691f56183
Prashant Kamdar <prashantkamdar@gmail.com>
2016-10-09 20:30:23 +0530

    compare the triplets

---
+++ CompareTheTriplets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alicePoints = 0;
+            int bobPoints = 0;
+
+            string aliceTriplet = Console.ReadLine();
+            string bobTriplet = Console.ReadLine();
+
+            string[] aliceSet = aliceTriplet.Split(' ');
+            string[] bobSet = bobTriplet.Split(' ');
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (Convert.ToInt32(aliceSet[i]) > Convert.ToInt32(bobSet[i]))
+                {
+                    alicePoints++;
+                }
+                else if (Convert.ToInt32(aliceSet[i]) < Convert.ToInt32(bobSet[i]))
+                {
+                    bobPoints++;
+                }
+            }
+
+            Console.WriteLine(alicePoints + " " + bobPoints);
+        }
+    }
+}
