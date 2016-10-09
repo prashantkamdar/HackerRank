835a94ba00a68a0d31e574f1462f30ac5250c23e
Prashant Kamdar <prashantkamdar@gmail.com>
2016-10-09 18:39:41 +0530

    conditional operators

--- ConditionalStatements/Program.cs
+++ ConditionalStatements/Program.cs
@@ -10,7 +10,27 @@ namespace ConditionalStatements
     {
         static void Main(string[] args)
         {
-            int t = Convert.ToInt32(Console.ReadLine());
+            int N = Convert.ToInt32(Console.ReadLine());
+            string message = "";
+
+            if (N % 2 != 0)
+            {
+                message = "Weird";
+            }
+            else if (N >= 2 && N <= 5)
+            {
+                message = "Not Weird";
+            }
+            else if (N >= 6 && N <= 20)
+            {
+                message = "Weird";
+            }
+            else if (N > 20)
+            {
+                message = "Not Weird";
+            }
+
+            Console.WriteLine(message);
         }
     }
 }
