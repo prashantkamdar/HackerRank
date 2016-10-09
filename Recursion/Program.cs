2be6a1053327e130869cc41e08a9bb8109240fa8
Prashant Kamdar <prashantkamdar@gmail.com>
2016-10-09 20:05:00 +0530

    recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNumber = Convert.ToInt32(Console.ReadLine());
+            int factorialNumber = factorial(baseNumber);
+            Console.WriteLine(factorialNumber);
+            Console.ReadLine();
+        }
+
+        public static int factorial(int x)
+        {
+            int calc = 1;
+
+            if (x > 1)
+            {
+                calc = x * factorial(x - 1);
+            }
+
+            return calc;
+        }
+    }
+}
