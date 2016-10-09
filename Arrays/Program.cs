2d8098a0a5188286e21ae6f07909a60d1776fcdf
Prashant Kamdar <prashantkamdar@gmail.com>
2016-10-09 19:39:06 +0530

    arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            Console.WriteLine(String.Join(" ", arr.Reverse()));
+        }
+    }
+}
