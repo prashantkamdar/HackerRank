ecf6cc23becd907e7998fa66909094780cf9140c
Prashant Kamdar <prashantkamdar@gmail.com>
2016-10-09 19:30:20 +0530

    letsreview

---
+++ LetsReview/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberofLines = Convert.ToInt32(Console.ReadLine());
+            string[] lineStrings = new string[numberofLines];
+
+            for (int i = 0; i < numberofLines; i++)
+            {
+                lineStrings[i]  = Console.ReadLine();
+            }
+
+            foreach (string s in lineStrings)
+            {
+                var evens = s.Where((item, index) => index % 2 == 0).ToArray();
+                var odds = s.Where((item, index) => index % 2 != 0).ToArray();
+
+                Console.WriteLine($"{new string(evens)} {new string(odds)}");
+            }
+        }
+    }
+}
