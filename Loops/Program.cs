9e8d939a1812d8a4ce27a364c17d58e12704624f
Prashant Kamdar <prashantkamdar@gmail.com>
2016-10-09 19:00:51 +0530

    Loops

---
+++ Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i= 1;i <= 10; i++)
+            {
+                Console.WriteLine($"{n} x {i} = {n*i}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
