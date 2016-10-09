e4511c1d987eeeab7b5707578c87457c55981b72
Prashant Kamdar <prashantkamdar@gmail.com>
2016-10-09 20:16:15 +0530

    binary numbers

---
+++ BinaryNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string binary = Convert.ToString(n, 2);
+
+            string[] oneStrings = binary.Split('0');
+
+            string longest = oneStrings.OrderByDescending(x => x.Length).First();
+            Console.WriteLine(longest.Length);
+        }
+    }
+}
